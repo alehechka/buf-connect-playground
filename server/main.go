a01df833dd35c063a83d3cac772981c4705c7717
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-08-08 11:57:45 -0500

    refactor and simplify grpc server

--- server/main.go
+++ server/main.go
@@ -1,22 +1,15 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"log"
 	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/alehechka/buf-connect-playground/proto/gen/users/v1/usersv1connect"
 	"github.com/alehechka/buf-connect-playground/services/users"
 	"github.com/alehechka/buf-connect-playground/utils"
 	"github.com/alehechka/buf-connect-playground/utils/database"
+	"github.com/alehechka/buf-connect-playground/utils/grpc"
 	"github.com/alehechka/buf-connect-playground/utils/otel"
-	"github.com/bufbuild/connect-go"
-	"github.com/rs/cors"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
@@ -30,76 +23,8 @@ func main() {
 	utils.Check(err)
 	defer disconnect()
 
-	mux := http.NewServeMux()
+	api := http.NewServeMux()
+	api.Handle(usersv1connect.NewUsersServiceHandler(users.NewServer()))
 
-	compress1KB := connect.WithCompressMinBytes(1024)
-	mux.Handle(usersv1connect.NewUsersServiceHandler(
-		users.NewServer(),
-		compress1KB,
-	))
-
-	addr := "localhost:8080"
-	if port := os.Getenv("PORT"); port != "" {
-		addr = ":" + port
-	}
-	srv := &http.Server{
-		Addr: addr,
-		Handler: h2c.NewHandler(
-			newCORS().Handler(mux),
-			&http2.Server{},
-		),
-		ReadHeaderTimeout: time.Second,
-		ReadTimeout:       5 * time.Minute,
-		WriteTimeout:      5 * time.Minute,
-		MaxHeaderBytes:    8 * 1024, // 8KiB
-	}
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		log.Printf("gRPC listening on: %s", addr)
-		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			log.Fatalf("HTTP listen and serve: %v", err)
-		}
-	}()
-
-	<-signals
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatalf("HTTP shutdown: %v", err) // nolint:gocritic
-	}
-}
-
-func newCORS() *cors.Cors {
-	// To let web developers play with the demo service from browsers, we need a
-	// very permissive CORS setup.
-	return cors.New(cors.Options{
-		AllowedMethods: []string{
-			http.MethodHead,
-			http.MethodGet,
-			http.MethodPost,
-			http.MethodPut,
-			http.MethodPatch,
-			http.MethodDelete,
-		},
-		AllowOriginFunc: func(origin string) bool {
-			// Allow all origins, which effectively disables CORS.
-			return true
-		},
-		AllowedHeaders: []string{"*"},
-		ExposedHeaders: []string{
-			// Content-Type is in the default safelist.
-			"Accept",
-			"Accept-Encoding",
-			"Accept-Post",
-			"Connect-Accept-Encoding",
-			"Connect-Content-Encoding",
-			"Content-Encoding",
-			"Grpc-Accept-Encoding",
-			"Grpc-Encoding",
-			"Grpc-Message",
-			"Grpc-Status",
-			"Grpc-Status-Details-Bin",
-		},
-	})
+	http.ListenAndServe(":"+os.Getenv("PORT"), h2c.NewHandler(grpc.NewCORS().Handler(api), &http2.Server{}))
 }
